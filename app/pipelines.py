37f07d80451e764861f0c21122a19075b0407b8c
Nick Harvey <nickharveyonline@gmail.com>
2021-06-26 09:44:51 -0400

    test

--- app/pipelines.py
+++ app/pipelines.py
@@ -4,18 +4,17 @@ import python_pachyderm
 
 # Connects to a pachyderm cluster on localhost:30650.
 # For other options, see the API docs.
-
+pach_client = python_pachyderm.Client()
 
 
 class Jaruco():
 
-	client = python_pachyderm.Client()
 	def general_restore(uploaded_file):
 		"""Do a general restore on a photo that doesn't have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
-		with client.commit("general_restore_input", "master") as commit:
-		    client.put_files(uploaded_file, commit, filename)
+		with pach_client.commit("general_restore_input", "master") as commit:
+		    pach_client.put_files(uploaded_file, commit, filename)
 		pass
 
 
@@ -23,6 +22,6 @@ class Jaruco():
 		"""Do a general restore on a photo that does have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
-		with client.commit("general_restore_w_cracks_input", "master") as commit:
-		    client.put_files(uploaded_file, commit, filename)
+		with pach_client.commit("general_restore_w_cracks_input", "master") as commit:
+		    pach_client.put_files(uploaded_file, commit, filename)
 		pass
