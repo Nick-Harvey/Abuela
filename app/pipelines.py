7680a9c0fd64a616fc01d12b814c255190ddf96c
Nick Harvey <nickharveyonline@gmail.com>
2021-06-25 23:32:47 -0400

    this time i'll use the right method

--- app/pipelines.py
+++ app/pipelines.py
@@ -13,7 +13,7 @@ class Jaruco():
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_input", "master") as commit:
-		    client.put_file_from_bytes(commit, filename, b"img_bytes")
+		    client.put_file_bytes(commit, filename, b"img_bytes")
 		pass
 
 
@@ -22,5 +22,5 @@ class Jaruco():
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_w_cracks_input", "master") as commit:
-		    client.put_file_from_bytes(commit, filename, b"img_bytes")
+		    client.put_file_bytes(commit, filename, b"img_bytes")
 		pass
