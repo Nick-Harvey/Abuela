dcae2d7f97d8eb1bdf0a103f7f5ddf837d073ba7
Nick Harvey <nickharveyonline@gmail.com>
2021-06-25 23:28:56 -0400

    Lol, they just got to the alien/ufo scene.

--- app/pipelines.py
+++ app/pipelines.py
@@ -11,7 +11,7 @@ class Jaruco():
 	def general_restore(uploaded_file):
 		"""Do a general restore on a photo that doesn't have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
-		img_bytes = uploaded_file.get_value()
+		img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_input", "master") as commit:
 		    client.put_file_from_bytes(commit, filename, b"img_bytes")
 		pass
@@ -20,7 +20,7 @@ class Jaruco():
 	def general_restore_wcracks(uploaded_file):
 		"""Do a general restore on a photo that does have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
-		img_bytes = uploaded_file.get_value()
+		img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_w_cracks_input", "master") as commit:
 		    client.put_file_from_bytes(commit, filename, b"img_bytes")
 		pass
