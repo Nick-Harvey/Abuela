bc6f14e9bbfb274d8fda979ac7a6421d40d58718
Nick Harvey <nickharveyonline@gmail.com>
2021-06-25 22:58:09 -0400

    test

--- app/pipelines.py
+++ app/pipelines.py
@@ -8,18 +8,17 @@ client = python_pachyderm.Client()
 
 class Jaruco():
 
-	def general_restore(image):
+	def general_restore(uploaded_file):
 		"""Do a general restore on a photo that doesn't have cracks"""
-		bytes_data = image.getvalue()
-		print(bytes_data)
+		bytes_data = uploaded_file.getvalue()
 		with client.commit("general_restore_input", "master") as commit:
-		    client.put_file_bytes(commit, "/", bytes_data)
+		    client.put_file_bytes(commit, "/", b"bytes_data")
 		pass
 
 
-	def general_restore_wcracks(image):
+	def general_restore_wcracks(uploaded_file):
 		"""Do a general restore on a photo that does have cracks"""
-		bytes_data = image.getvalue()
+		bytes_data = uploaded_file.getvalue()
 		with client.commit("general_restore_w_cracks_input", "master") as commit:
-		    client.put_file_bytes(commit, "/", bytes_data)
+		    client.put_file_bytes(commit, "/", b"bytes_data")
 		pass
