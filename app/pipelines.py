1eb7c4f52ff7cccbfdca8d56f11112c576d445fb
Nick Harvey <nickharveyonline@gmail.com>
2021-06-26 09:39:23 -0400

    test

--- app/pipelines.py
+++ app/pipelines.py
@@ -14,7 +14,7 @@ class Jaruco():
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_input", "master") as commit:
-		    client.put_file(uploaded_file, commit, filename)
+		    client.put_files(uploaded_file, commit, filename)
 		pass
 
 
@@ -23,5 +23,5 @@ class Jaruco():
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_w_cracks_input", "master") as commit:
-		    client.put_file(uploaded_file, commit, filename)
+		    client.put_files(uploaded_file, commit, filename)
 		pass
