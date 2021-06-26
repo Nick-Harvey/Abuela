6afbc3dbfae53ac38fdec1fec513bea7a4af8a30
Nick Harvey <nickharveyonline@gmail.com>
2021-06-26 09:41:15 -0400

    late night coding goof

--- app/pipelines.py
+++ app/pipelines.py
@@ -4,11 +4,12 @@ import python_pachyderm
 
 # Connects to a pachyderm cluster on localhost:30650.
 # For other options, see the API docs.
-client = python_pachyderm.Client()
+
 
 
 class Jaruco():
 
+	client = python_pachyderm.Client()
 	def general_restore(uploaded_file):
 		"""Do a general restore on a photo that doesn't have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
