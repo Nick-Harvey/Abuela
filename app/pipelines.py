6f3fdcdbd576707025da9cd3fc091e75cedfe407
Nick Harvey <nickharveyonline@gmail.com>
2021-06-26 00:10:47 -0400

    test

--- app/pipelines.py
+++ app/pipelines.py
@@ -14,7 +14,7 @@ class Jaruco():
 		filename = '/{}'.format(uploaded_file.name)
 		#img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_input", "master") as commit:
-		    client.put_file_from_fileobj(commit, filename, uploaded_file)
+		    python_pachyderm.put_file_from_fileobj(commit, filename, uploaded_file)
 		pass
 
 
@@ -23,7 +23,7 @@ class Jaruco():
 		filename = '/{}'.format(uploaded_file.name)
 		#img_bytes = uploaded_file.getvalue()
 		with client.commit("general_restore_w_cracks_input", "master") as commit:
-		    client.put_file_from_fileobj(commit, filename, uploaded_file)
+		    python_pachyderm.put_file_from_fileobj(commit, filename, uploaded_file)
 		pass
 
 
