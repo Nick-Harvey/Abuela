c71accbd9bdc2c968ae26cd8c0f9db7de66fd740
Nick Harvey <nickharveyonline@gmail.com>
2021-06-25 22:47:38 -0400

    watching spys like us and writing code is not a good idea, lol. hopefully fixing put files

--- app/pipelines.py
+++ app/pipelines.py
@@ -10,15 +10,15 @@ class Jaruco():
 
 	def general_restore(image):
 		"""Do a general restore on a photo that doesn't have cracks"""
-		
+		bytes_data = image.getvalue()
 		with client.commit("general_restore_input", "master") as commit:
-		    client.put_file_bytes(commit, "/", image)
+		    client.put_file_bytes(commit, "/", b"bytes_data")
 		pass
 
 
 	def general_restore_wcracks(image):
 		"""Do a general restore on a photo that does have cracks"""
-		
+		bytes_data = image.getvalue()
 		with client.commit("general_restore_w_cracks_input", "master") as commit:
-		    client.put_file_bytes(commit, "/", image)
+		    client.put_file_bytes(commit, "/", b"bytes_data")
 		pass
